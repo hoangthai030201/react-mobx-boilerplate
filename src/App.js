3b86cac4a7b0d4e30435bb825c51dcd6ea4f70d9
Danny Hoang <hnthai@humaxdigital.com>
2019-08-13 10:15:25 +0700

    update config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/Test'
+import Test from 'components/Test'
 
 class App extends Component {
     render() {
