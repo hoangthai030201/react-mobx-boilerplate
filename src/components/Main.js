205ac971755bfa89dd0936bfb3837d2e3302093a
Danny Hoang <hnthai@humaxdigital.com>
2019-08-15 06:43:15 +0700

    add

---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { observable, action } from 'mobx';
+import { inject, observer } from 'mobx-react';
+
+import styled from 'styled-components'
+
+const Div = styled.div`
+
+`
+
+//@withRouter
+//@inject('commonStore')
+//@observer
+export default class Main extends Component {
+    render() {
+        return (
+            <Div>
+                Main
+            </Div>
+        );
+    }
+    
+}
+
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Login from './Login'
+import Home from './Home'
+import Pvs from './Pvs'
+
+export {
+    Login,
+    Home,
+    Pvs
+}
