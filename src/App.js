659b85cfebfcc6e006f6b8a0a76f2b116e011f2b
Danny Hoang <hnthai@humaxdigital.com>
2019-08-15 06:42:31 +0700

    l

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import logo from './logo.svg';
+import { Login } from 'components'
+
 import './App.css';
 import Test from './components/Test'
+import Main from './Main';
 
 class App extends Component {
+
     render() {
 
         return (
             <div className="App">
-                <Test />
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/login' component={Login}/>
+                        <Route path='/' component={Main}/>
+                    </Switch>
+                </BrowserRouter>
             </div>
         );
     }
